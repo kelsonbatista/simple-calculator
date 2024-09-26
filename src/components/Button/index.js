9f37dcf4af1344fe58f8f252abcbae2c48068488
Kelson <kelsonbatista@gmail.com>
2024-09-26 20:11:11 -0300

    Added button and display components

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+function Button(props) {
+  const { title } = props
+  
+  return (
+    <button>{title}</button>
+  )
+}
+
+export default Button
---
+++ src/components/Display/index.js
@@ -0,0 +1,9 @@
+function Display(props) {
+  const { title } = props
+  
+  return (
+    <div>Display { title }</div>
+  )
+}
+
+export default Display
